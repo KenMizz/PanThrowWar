f5675c66a1b43be8ee5630862f759102ce44da89
Ken Mizz <mizzken16@gmail.com>
2019-01-26 12:36:37 -0500

    增加use, 更新SessionTask

--- src/yl14/PanThrowWar/EventListener.php
+++ src/yl14/PanThrowWar/EventListener.php
@@ -2,7 +2,9 @@
 
 namespace yl14\PanThrowWar;
 
-use pocketmine\event\Listener;
+use pocketmine\event\{
+    Listener, player\PlayerJoinEvent, player\PlayerQuitEvent, player\PlayerDeathEvent, player\PlayerDropItemEvent, entity\EntityArmorChangeEvent, block\BlockBreakEvent, block\BlockPlaceEvent
+};
 
 class EventListener implements Listener {
 
--- src/yl14/PanThrowWar/SessionTask.php
+++ src/yl14/PanThrowWar/SessionTask.php
@@ -13,6 +13,7 @@ class SessionTask extends Task {
 
     private $explodetime;
     private $gametime;
+    private $countdown = 10;
     
     public function __construct(PanThrowWar $plugin, int $roomid) {
         $this->plugin = $plugin;
@@ -35,14 +36,30 @@ class SessionTask extends Task {
                     if(count($Session->getPlayers()) >= $Session->getMinPlayer()) {
                         $Session->setStatus(1);
                         $this->plugin->updateSession($this->roomid, $Session);
+                        $this->plugin->getServer()->broadcastMessage($this->plugin->prefix."房间人数已到达最小可玩人数，进入准备状态", $Session->getPlayers());
                     }
                     if(count($Session->getPlayers() <= 0)) {
+                        $this->plugin->getScheduler()->cancelTask($this->getTaskId());
                         $this->plugin->removeRoom($this->roomid);
                     }
                     $this->plugin->getServer()->broadcastTip(TF::YELLOW."等待玩家中...", $Session->getPlayers());
                 break;
 
                 case 1:
+                    $this->countdown--;
+                    $this->plugin->getServer()->broadcastTitle($this->countdown, "", -1, -1, -1, $Session->getPlayers());
+                    if($this->countdown == 0) {
+                        $Session->setStatus(2);
+                        $this->plugin->updateSession($this->roomid, $Session);
+                        $this->plugin->getServer()->broadcastTip(TF::YELLOW."游戏开始咯!", $Session->getPlayers());
+                    }
+                    if(count($Session->getPlayers() <= 0)) {
+                        $this->plugin->getScheduler()->cancelTask($this->getTaskId());
+                        $this->plugin->removeRoom($this->roomid);
+                    }
+                break;
+
+                case 2:
                     //TODO
             }
         }
