bf10edd9a2f404de797ab7934cd7cea2785bf4cb
KenMizz <mizzken16@gmail.com>
2019-02-24 15:54:17 -0500

    add PTWTask

---
+++ src/yl14/PanThrowWar/PTWTask.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace yl14\PanThrowWar;
+
+use pocketmine\scheduler\Task;
+
+class PTWTask extends Task {
+
+    private $plugin;
+    private $sessionid;
+
+    public function __construct(PanThrowWar $plugin, int $sessionid) {
+        $this->plugin = $plugin;
+        $this->sessionid = $sessionid;
+    }
+
+    public function onRun(int $currentTick) {
+        //TODO
+    }
+}
