7c13893ab18445f41d4ed1124331ce915ca7a683
KenMizz <mizzken16@gmail.com>
2019-02-24 21:33:43 -0500

    implementing Task(WIP)

--- src/yl14/PanThrowWar/PTWTask.php
+++ src/yl14/PanThrowWar/PTWTask.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace yl14\PanThrowWar;
 
 use pocketmine\scheduler\Task;
+use pocketmine\utils\TextFormat as TF;
 
 class PTWTask extends Task {
 
@@ -19,7 +20,27 @@ class PTWTask extends Task {
     public function onRun(int $currentTick) {
         $Session = $this->plugin->getRoomById($this->sessionid);
         if($Session instanceof PTWSession) {
-            
+            $Status = $Session->getStatus();
+            switch($Status) {
+
+                case 0:
+                    $this->plugin->getServer()->broadcastTip(TF::YELLOW."等待玩家中...", $Session->getPlayers());
+                    if(count($Session->getPlayers()) >= $Session->getMaxPlayer()) {
+                        $Session->setStatus(1);
+                        $this->plugin->updateSession($Session->getSessionId(), $Session);
+                    }
+                    if(count($Session->getPlayers()) <= 0) {
+                        $this->plugin->closeRoom($Session->getSessionId(), $Session->getTaskId());
+                    }
+                break;
+
+                case 1:
+                    //TODO
+                break;
+
+                case 2:
+                    //TODO
+            }
         }
     }
 }
