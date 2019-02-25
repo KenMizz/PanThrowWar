7efb49742f01774d4a18be08dfc31abd91214c8a
KenMizz <mizzken16@gmail.com>
2019-02-24 23:15:43 -0500

    完善事件

--- src/yl14/PanThrowWar/EventListener.php
+++ src/yl14/PanThrowWar/EventListener.php
@@ -8,15 +8,63 @@ use pocketmine\event\Listener;
 use pocketmine\event\{
     player\PlayerQuitEvent, player\PlayerDropItemEvent, player\PlayerItemHeldEvent, entity\EntityArmorChangeEvent, entity\EntityDamageByEntityEvent, block\BlockPlaceEvent, block\BlockBreakEvent
 };
+use pocketmine\Player;
 
 class EventListener implements Listener {
 
     private $plugin;
+    private $onQuit = [];
 
     public function __construct(PanThrowWar $plugin) {
         $this->plugin = $plugin;
     }
 
-    
+    public function onPlayerQuit(PlayerQuitEvent $ev) {
+        $player = $ev->getPlayer();
+        if($this->plugin->getPlayerInGame($player)) {
+            $this->plugin->leaveRoom([$player], $this->plugin->getPlayerInGame($player), 1);
+            unset($this->onQuit[$player->getName()]);
+        }
+    }
+
+    public function onPlayerDropItem(PlayerDropItemEvent $ev) {
+        $player = $ev->getPlayer();
+        if($this->plugin->getPlayerInGame($player)) {
+            $ev->setCancelled();
+        }
+    }
+
+    public function onBlockPlace(BlockPlaceEvent $ev) {
+        $player = $ev->getPlayer();
+        if($this->plugin->getPlayerInGame($player)) {
+            $ev->setCancelled();
+        }
+    }
+
+    public function onBlockBreak(BlockBreakEvent $ev) {
+        $player = $ev->getPlayer();
+        if($this->plugin->getPlayerInGame($player)) {
+            $ev->setCancelled();
+        }
+    }
+
+    public function onPlayerItemHeld(PlayerItemHeldEvent $ev) {
+        $Item = $ev->getItem();
+        $player = $ev->getPlayer();
+        $exitwool = Item::get(Item::WOOL);
+        $exitwool->setCustomName("离开房间");
+        if($this->plugin->getPlayerInGame($player)) {
+            if($Item == $exitwool) {
+                if(!isset($this->onQuit[$player->getName()])) {
+                    $ev->setCancelled();
+                    $this->onQuit[$player->getName()] = $player->getName();
+                    $player->sendMessage("再切换一次即可退出房间");
+                } else {
+                    unset($this->onQuit[$player->getName()]);
+                    $this->plugin->leaveRoom([$player], $this->plugin->getPlayerInGame($player), 1);
+                }
+            }
+        }
+    }
 }
 
