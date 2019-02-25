d08ae01b46b6d2cfb38401586ba8423a85a28940
KenMizz <mizzken16@gmail.com>
2019-02-24 21:54:29 -0500

    add needed events

--- src/yl14/PanThrowWar/EventListener.php
+++ src/yl14/PanThrowWar/EventListener.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace yl14\PanThrowWar;
 
 use pocketmine\event\Listener;
+use pocketmine\event\{
+    player\PlayerQuitEvent, player\PlayerDropItemEvent, player\PlayerItemHeldEvent, entity\EntityArmorChangeEvent, entity\EntityDamageByEntityEvent, block\BlockPlaceEvent, block\BlockBreakEvent
+};
 
 class EventListener implements Listener {
 
