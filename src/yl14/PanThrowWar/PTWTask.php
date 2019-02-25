0c9ad89cf20d05dac1806e21bac8a8ecc0a610cb
Ken Mizz <mizzken16@gmail.com>
2019-02-25 14:35:11 -0500

    继续完善Task

--- src/yl14/PanThrowWar/PTWTask.php
+++ src/yl14/PanThrowWar/PTWTask.php
@@ -6,11 +6,14 @@ namespace yl14\PanThrowWar;
 
 use pocketmine\scheduler\Task;
 use pocketmine\utils\TextFormat as TF;
+use pocketmine\Player;
 
 class PTWTask extends Task {
 
     private $plugin;
     private $sessionid;
+
+    private $readyCountdown = 10;
 
     public function __construct(PanThrowWar $plugin, int $sessionid) {
         $this->plugin = $plugin;
@@ -25,7 +28,8 @@ class PTWTask extends Task {
 
                 case 0:
                     $this->plugin->getServer()->broadcastTip(TF::YELLOW."等待玩家中...", $Session->getPlayers());
-                    if(count($Session->getPlayers()) >= $Session->getMaxPlayer()) {
+                    if(count($Session->getPlayers()) >= $Session->getMinPlayer()) {
+                        $this->plugin->getServer()->broadcastMessage(TF::YELLOW."房间人数足够啦！正在开始游戏...");
                         $Session->setStatus(1);
                         $this->plugin->updateSession($Session->getSessionId(), $Session);
                     }
@@ -35,11 +39,45 @@ class PTWTask extends Task {
                 break;
 
                 case 1:
-                    //TODO
+                    $this->readyCountdown--;
+                    switch($this->readyCountdown) {
+
+                        default:
+                            $this->plugin->getServer()->broadcastTitle((string)$this->readyCountdown, "", -1, -1, -1, $Session->getPlayers());
+                        break;
+
+                        case 5:
+                        case 4:
+                        case 3:
+                        case 2:
+                            $this->plugin->getServer()->broadcastTitle(TF::YELLOW.(string)$this->readyCountdown, "", -1, -1, -1, $Session->getPlayers());
+                        break;
+
+                        case 1:
+                            $this->plugin->getServer()->broadcastTitle(TF::YELLOW."开始游戏!", "", -1, -1, -1, $Session->getPlayers());
+                        break;
+
+                        case 0:
+                            foreach($Session->getPlayers() as $player) {
+                                if($player instanceof Player) {
+                                    $player->setXpLevel(0);
+                                }
+                            }
+                            $Session->setStatus(2);
+                            $this->plugin->updateSession($Session->getSessionId(), $Session);
+                    }
+                    foreach($Session->getPlayers() as $player) {
+                        if($player instanceof Player) {
+                            $player->setXpLevel($this->readyCountdown);
+                        }
+                    }
+                    if(count($Session->getPlayers()) <= 0) {
+                        $this->plugin->closeRoom($Session->getSessionId(), $Session->getTaskId());
+                    }
                 break;
 
                 case 2:
-                    //TODO
+                    
             }
         }
     }
