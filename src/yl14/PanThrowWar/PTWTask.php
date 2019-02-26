e5b821693a82fe0721658cece41fcd3fe4e01c3f
Ken Mizz <mizzken16@gmail.com>
2019-02-25 20:17:10 -0500

    implementing Task

--- src/yl14/PanThrowWar/PTWTask.php
+++ src/yl14/PanThrowWar/PTWTask.php
@@ -7,17 +7,33 @@ namespace yl14\PanThrowWar;
 use pocketmine\scheduler\Task;
 use pocketmine\utils\TextFormat as TF;
 use pocketmine\Player;
+use pocketmine\item\Item;
+use pocketmine\entity\EffectInstance;
+use pocketmine\entity\Effect;
 
 class PTWTask extends Task {
 
     private $plugin;
-    private $sessionid;
+    private $sessionid = 0;
 
     private $readyCountdown = 10;
+    private $switchingtime = 0;
+    private $explodetime = 0;
+    private $onSwitching = true;
+    private $explodeCountdown = false;
 
     public function __construct(PanThrowWar $plugin, int $sessionid) {
         $this->plugin = $plugin;
         $this->sessionid = $sessionid;
+        $this->init();
+    }
+
+    private function init() {
+        $Session = $this->plugin->getRoomById($this->sessionid);
+        if($Session instanceof PTWSession) {
+            $this->switchingtime = $Session->getSwitchingTime();
+            $this->explodetime = $Session->getExplodeTime();
+        }
     }
 
     public function onRun(int $currentTick) {
@@ -29,7 +45,7 @@ class PTWTask extends Task {
                 case 0:
                     $this->plugin->getServer()->broadcastTip(TF::YELLOW."等待玩家中...", $Session->getPlayers());
                     if(count($Session->getPlayers()) >= $Session->getMinPlayer()) {
-                        $this->plugin->getServer()->broadcastMessage(TF::YELLOW."房间人数足够啦！正在开始游戏...");
+                        $this->plugin->getServer()->broadcastMessage(TF::YELLOW."房间人数足够啦！正在开始游戏...", $Session->getPlayers());
                         $Session->setStatus(1);
                         $this->plugin->updateSession($Session->getSessionId(), $Session);
                     }
@@ -63,6 +79,7 @@ class PTWTask extends Task {
                                     $player->setXpLevel(0);
                                 }
                             }
+                            $this->plugin->getServer()->broadcastMessage($this->plugin->prefix."将在{$Session->getSwitchingTime()}秒后随机抽取玩家背锅", $Session->getPlayers());
                             $Session->setStatus(2);
                             $this->plugin->updateSession($Session->getSessionId(), $Session);
                     }
@@ -77,7 +94,47 @@ class PTWTask extends Task {
                 break;
 
                 case 2:
-                    
+                    $this->switchingtime--;
+                    if($this->switchingtime == 0) {
+                        $this->switchingtime = $Session->getSwitchingTime();
+                        if($this->onSwitching) {
+                            foreach($Session->getPlayers() as $player) {
+                                if($player instanceof Player) {
+                                    if(!$Session->isSpectator($player)) {
+                                        $player->getArmorInventory()->clearAll();
+                                        $player->getArmorInventory()->setHelmet(Item::get(Item::MOB_HEAD, 4));
+                                        $player->removeAllEffects();
+                                        $player->addEffect(new EffectInstance(Effect::getEffect(Effect::SPEED), 300 * 900, 1, false));
+                                        $player->sendMessage($this->plugin->prefix."你背上了沉重的锅，快传给别人吧");
+                                        $this->plugin->getServer()->broadcastMessage($this->plugin->prefix."{$player->getName()}成功背锅，快跑丫", $Session->getPlayers());
+                                        $this->plugin->getServer()->broadcastMessage($this->plugin->prefix."锅子还有{$Session->getExplodeTime()}后爆炸", $Session->getPlayers());
+                                        $this->explodeCountdown = true;
+                                        $this->onSwitching = false;
+                                    }
+                                    continue;
+                                }
+                            }
+                        }
+                    }
+                    if($this->explodeCountdown) {
+                        $this->explodeCountdown--;
+                        if($this->explodeCountdown == 10) {
+                            $this->plugin->getServer()->broadcastMessage($this->plugin->prefix."锅子还有10秒后爆炸！", $Session->getPlayers());
+                        }
+                        if($this->explodeCountdown == 5) {
+                            $this->plugin->getServer()->broadcastMessage($this->plugin->prefix."锅子还有5秒后爆炸！", $Session->getPlayers());
+                        }
+                        if($this->explodetime == 0) {
+                            $this->explodetime = $Session->getExplodeTime();
+                            foreach($Session->getPlayers() as $player) {
+                                if($player instanceof Player) {
+                                    if($player->getArmorInventory()->getHelmet() == Item::get(Item::MOB_HEAD, 4)) { //锅子携带者
+
+                                    }
+                                }
+                            }
+                        }
+                    }
             }
         }
     }
